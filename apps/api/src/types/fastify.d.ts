8c8ecc61c775058983b5f5a0bd2071b7a9ce1a6e
John Boyer <joeRob2468@yahoo.com>
2026-01-26 16:45:43 -0600

    fix(api): add correct jwt/user types to fastify type declaration

--- apps/api/src/types/fastify.d.ts
+++ apps/api/src/types/fastify.d.ts
@@ -3,18 +3,20 @@ import { OAuth2Namespace } from "@fastify/oauth2";
 import { JWT } from "@fastify/jwt";
 import * as schema from "@/db/schema";
 
+declare module '@fastify/jwt' {
+  interface FastifyJWT {
+    user: {
+      id: string;
+      email: string;
+    }
+  }
+}
+
 declare module 'fastify' {
   interface FastifyInstance {
     db: PostgresJsDatabase<typeof schema>;
     google: OAuth2Namespace;
     jwt: JWT;
     authenticate: (req: FastifyRequest, res: FastifyReply) => Promise<void>;
-  }
-
-  interface FastifyRequest {
-    user: {
-      id: string;
-      email: string;
-    };
   }
 }
