5f11fade75b6f67faf5f46c75604c0027250ab7d
John Boyer <joeRob2468@yahoo.com>
2026-01-26 15:23:00 -0600

    refactor(api): move fastify types to separate declaration file

--- apps/api/src/db/schema.ts
+++ apps/api/src/db/schema.ts
@@ -1,5 +1,6 @@
 import { User } from "@repo/common";
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { createSelectSchema } from 'drizzle-zod';
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
@@ -8,5 +9,5 @@ export const users = pgTable('users', {
   createdAt: timestamp('created_at').defaultNow().notNull()
 }) satisfies Record<keyof User, unknown>;
 
+export const selectUserSchema = createSelectSchema(users);
 export type DbUser = typeof users.$inferSelect;
-export type NewDbUser = typeof users.$inferInsert;
--- apps/api/src/plugins/drizzle.ts
+++ apps/api/src/plugins/drizzle.ts
@@ -4,12 +4,6 @@ import postgres from "postgres";
 import * as schema from "@/db/schema";
 import { env } from "@repo/env";
 
-declare module 'fastify' {
-  interface FastifyInstance {
-    db: ReturnType<typeof drizzle<typeof schema>>
-  }
-}
-
 export default fp(async (fastify) => {
   const queryClient = postgres(env.DATABASE_URL);
   const db = drizzle(queryClient, { schema });
