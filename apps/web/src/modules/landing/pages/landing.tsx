de8818e170197e3ff584c41735dae02f6c99fb5e
John Boyer <joeRob2468@yahoo.com>
2026-03-09 18:16:17 -0500

    feat(web): automatically redirect to /dashboard from landing page when user is already logged in

--- apps/web/src/modules/landing/pages/landing.tsx
+++ apps/web/src/modules/landing/pages/landing.tsx
@@ -1,12 +1,20 @@
+import { useUser } from '@/modules/auth/hooks/use-user';
 import { Button, Container, Stack, Text, Title, rem } from '@mantine/core';
-import { IconBrandGoogle } from '@tabler/icons-react';
 import { env } from '@repo/env';
+import { IconBrandGoogle } from '@tabler/icons-react';
+import { Navigate } from 'react-router-dom';
 
 export const Landing = () => {
+  const { user, isLoading } = useUser();
+
   const handleLogin = () => {
     const returnUrl = `${window.location.origin}/dashboard`;
     window.location.href = `${env.VITE_API_URL}/auth/login/google?return_url=${encodeURIComponent(returnUrl)}`;
   };
+
+  if (user) {
+    return <Navigate to="/dashboard" replace />;
+  }
 
   return (
     <Container size="md" h="100vh" display="flex" style={{ alignItems: 'center', justifyContent: 'center' }}>
@@ -20,7 +28,14 @@ export const Landing = () => {
           real-time. No clutter, just groceries.
         </Text>
 
-        <Button size="xl" variant="default" leftSection={<IconBrandGoogle size={20} />} onClick={handleLogin} mt="xl">
+        <Button
+          size="xl"
+          variant="default"
+          leftSection={<IconBrandGoogle size={20} />}
+          onClick={handleLogin}
+          loading={isLoading}
+          mt="xl"
+        >
           Continue with Google
         </Button>
       </Stack>
